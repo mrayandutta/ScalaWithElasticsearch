c9841d1487776703dac4fe0b25cc1c33f53b6269
Ayan Dutta <mrayandutta@gmail.com>
2016-11-02 19:51:32 +0530

    Added Get

--- src/main/scala/sparkwithes/ESandScalaApp.scala
+++ src/main/scala/sparkwithes/ESandScalaApp.scala
@@ -3,8 +3,7 @@ package sparkwithes
 import org.elasticsearch.action.get.GetResponse
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.transport.InetSocketTransportAddress
-
-import scala.io.Source
+import collection.mutable.HashMap
 
 /**
   * Created by Ayan on 10/31/2016.
@@ -13,6 +12,12 @@ object ESandScalaApp extends App
 {
   val transportClient = new TransportClient();
   transportClient.addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
+  val index ="a1"
+  val typeStr ="b1"
+  val id ="1011"
+
+  //indexJson(index,typeStr,id)
+  getRecord(index,typeStr,id)
 
   /*
   val jsons = Source.fromFile("src/main/resources/sample.json").getLines().toList
@@ -22,14 +27,22 @@ object ESandScalaApp extends App
   }
   */
 
-  def indexJson(json: String, id: String) =
+  def indexJson(index: String,typeStr: String,id: String) =
   {
-    transportClient.prepareIndex("esa", "activityStream", id).setSource(json).execute().actionGet()
+    val data: HashMap[String, Object] = HashMap("title"->"t1","content"-> "c1","postDate"-> "p1")
+    transportClient.prepareIndex(index, typeStr, id).setSource(data,"").execute().actionGet()
   }
 
   def get(json: String, id: String) =
   {
     //GetResponse res = transportClient.prepareGet("esa", "activityStream", id).execute().actionGet()
-    GetResponse getResponse = transportClient.prepareGet("kodcucom", "article", "1").execute().actionGet()
+     val response: GetResponse = transportClient.prepareGet("kodcucom", "article", "1").execute().actionGet()
+  }
+
+  def getRecord(index: String,typeStr: String ,id: String) =
+  {
+    val response: GetResponse = transportClient.prepareGet(index, typeStr, id).execute().actionGet()
+    print("response :"+response.getSource())
+
   }
 }
